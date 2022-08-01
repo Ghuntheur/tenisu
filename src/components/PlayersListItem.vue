ec69c12b0327d86e66119a1efea5b83639146517
Florian <zobeleflorian@gmail.com>
2022-08-01 03:33:22 +0200

    feat: image

--- src/components/PlayersListItem.vue
+++ src/components/PlayersListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="players-list-item-container">
     <section class="picture-container">
-      <!-- <img :src="player.picture" /> -->
+      <img :src="player.picture" />
     </section>
 
     <section class="data-container">
@@ -34,26 +34,34 @@ const playerName = computed(
 
 <style lang="scss" scoped>
 .players-list-item-container {
+  --picture-size: 34%;
+
+  position: relative;
   display: flex;
+  justify-content: flex-end;
   align-items: center;
   background: var(--color-light);
-  padding: var(--spacing-l) var(--spacing-xl) var(--spacing-l) 0;
+  padding-right: var(--spacing-l);
   box-shadow: 0 0 12px -4px var(--color-dark);
   cursor: pointer;
 
   .picture-container {
-    width: 25%;
+    width: var(--picture-size);
+    height: 150px;
+    justify-content: center;
+    align-items: flex-start;
     overflow: hidden;
 
     img {
       height: 100%;
-      width: 100%;
-      object-position: bottom;
+      width: 120%;
+      object-fit: cover;
+      object-position: -20px top;
     }
   }
 
   .data-container {
-    width: 100%;
+    width: calc(100% - var(--picture-size));
 
     .player-name {
       margin-bottom: var(--spacing-s);
